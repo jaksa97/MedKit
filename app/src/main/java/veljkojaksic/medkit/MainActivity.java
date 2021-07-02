d56300e3f4fc34849ed29de47a0975e03ffc7c21
jaksa97 <veljko.jaksic@pmf.edu.rs>
2021-07-02 06:41:08 +0200

    Added ImageSlider to MainActivity

--- app/src/main/java/veljkojaksic/medkit/MainActivity.java
+++ app/src/main/java/veljkojaksic/medkit/MainActivity.java
@@ -7,7 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.models.SlideModel;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageSlider imageSlider = findViewById(R.id.imageSlider);
         logout = findViewById(R.id.LogoutBtn);
 
         logout.setOnClickListener(new View.OnClickListener() {
@@ -28,5 +34,16 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        List<SlideModel> slideModels = new ArrayList<>();
+        slideModels.add(new SlideModel("https://cdn.graciousquotes.com/wp-content/uploads/2020/05/Take-care-of-your-body.-Its-the-only-place-you-have-to-live..jpg",""));
+        slideModels.add(new SlideModel("https://cdn.graciousquotes.com/wp-content/uploads/2020/05/A-healthy-outside-starts-from-the-inside..jpg",""));
+        slideModels.add(new SlideModel("https://cdn.graciousquotes.com/wp-content/uploads/2020/03/Health-is-like-money-we-never-have-a-true-idea-of-its-value-until-we-lose-it.jpg",""));
+        slideModels.add(new SlideModel("https://cdn.graciousquotes.com/wp-content/uploads/2020/03/If-you-truly-treat-your-body-like-a-temple-it-will-serve-you-well-for-decades.-If-you-abuse-it-you-must-be-prepared-for-poor-health-and-a-lack-of-energy.jpg",""));
+        slideModels.add(new SlideModel("https://cdn.graciousquotes.com/wp-content/uploads/2020/03/Healthy-citizens-are-the-greatest-asset-any-country-can-have.jpg",""));
+        slideModels.add(new SlideModel("https://cdn.graciousquotes.com/wp-content/uploads/2020/03/Your-body-will-be-around-a-lot-longer-than-that-expensive-handbag.-Invest-in-yourself.jpg",""));
+        slideModels.add(new SlideModel("https://cdn.graciousquotes.com/wp-content/uploads/2020/03/Good-health-is-not-something-we-can-buy.-However-it-can-be-an-extremely-valuable-savings-account.jpg",""));
+        imageSlider.setImageList(slideModels, true);
+
     }
 }
